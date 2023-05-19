64b62047359e740032a17fc443a671c6e9646ad7
mortalis13 <stangerroman@gmail.com>
2023-05-19 22:44:59 +0200

    update Q and db for specific filters only

--- filter_fx.h
+++ filter_fx.h
@@ -10,7 +10,13 @@ const double kSmallestNegativeFloatValue = -1.175494351e-38;        /* min negat
 const double kPi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899;
 
 
-enum class FilterAlgorithm { kLPF1, kHPF1, kLPF2, kHPF2, kBPF2, kBSF2, kLowShelf, kHiShelf, kNCQParaEQ, kCQParaEQ };
+enum class FilterAlgorithm {
+  kLPF1, kHPF1,
+  kLPF2, kHPF2,
+  kBPF2, kBSF2,
+  kLowShelf, kHiShelf,
+  kNCQParaEQ, kCQParaEQ
+};
 
 
 inline bool checkFloatUnderflow(double& value) {
@@ -41,8 +47,8 @@ public:
     calculateFilterCoeffs();
   }
   
-  void setFilterType(FilterAlgorithm algorithm) {
-    this->algorithm = algorithm;
+  void setFilterType(FilterAlgorithm type) {
+    this->algorithm = type;
     calculateFilterCoeffs();
   }
   
@@ -52,18 +58,43 @@ public:
   }
   
   void setQualityFactor(double qFactor) {
-    this->Q = qFactor;
-    calculateFilterCoeffs();
+    switch (algorithm) {
+      case FilterAlgorithm::kLPF2:
+      case FilterAlgorithm::kHPF2:
+      case FilterAlgorithm::kBPF2:
+      case FilterAlgorithm::kBSF2:
+      case FilterAlgorithm::kNCQParaEQ:
+      case FilterAlgorithm::kCQParaEQ:
+        this->Q = qFactor;
+        calculateFilterCoeffs();
+    }
   }
   
   void setDb(double db) {
-    this->db = db;
-    calculateFilterCoeffs();
+    switch (algorithm) {
+      case FilterAlgorithm::kLowShelf:
+      case FilterAlgorithm::kHiShelf:
+      case FilterAlgorithm::kNCQParaEQ:
+      case FilterAlgorithm::kCQParaEQ:
+        this->db = db;
+        calculateFilterCoeffs();
+    }
   }
   
   double processAudioSample(double xn);
 
 protected:
+  bool calculateFilterCoeffs();
+  
+  void resetCoeffs() {
+    cf_a0 = cf_a1 = cf_a2 = cf_b1 = cf_b2 = cf_c0 = cf_d0 = 0.0;
+  }
+  
+  void resetStates() {
+    x_z1 = x_z2 = y_z1 = y_z2 = 0.0;
+  }
+
+private:
   double cf_a0 = 0.0;
   double cf_a1 = 0.0;
   double cf_a2 = 0.0;
@@ -81,19 +112,10 @@ protected:
   double sampleRate = 44100.0;
   
   FilterAlgorithm algorithm;
+
   double fc = 100.0;
   double Q = 0.707;
   double db = 0.0;
-
-  bool calculateFilterCoeffs();
-  
-  void resetCoeffs() {
-    cf_a0 = cf_a1 = cf_a2 = cf_b1 = cf_b2 = cf_c0 = cf_d0 = 0.0;
-  }
-  
-  void resetStates() {
-    x_z1 = x_z2 = y_z1 = y_z2 = 0.0;
-  }
 };
 
 #endif //FILTER_FX_H
